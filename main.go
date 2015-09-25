88da5535dd1727f19d3688b05a30d7f0bcba1e72
Craig Swank <craigswank@gmail.com>
2015-09-25 03:37:32 +0000

    tidying up

--- main.go
+++ main.go
@@ -69,23 +69,12 @@ func main() {
 func start(db *bolt.DB, port, root string, iRoot string, lg controllers.Logger) {
 	auth.DB = db
 
-	i := mux.NewRouter()
-	i.HandleFunc("/internal/updates", Relay).Methods("POST")
-	http.Handle(iRoot, i)
-	a := fmt.Sprintf(":%s", os.Getenv("QUIMBY_INTERNAL_PORT"))
-	go func() {
-		lg.Printf("listening on %s", a)
-		err := http.ListenAndServe(a, i)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	go startInternal(iRoot, lg)
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/login", auth.Login).Methods("POST")
 	r.HandleFunc("/api/logout", auth.Logout).Methods("POST")
-
 	r.HandleFunc("/api/ping", Ping).Methods("GET")
 	r.HandleFunc("/api/users/current", GetUser).Methods("GET")
 	r.HandleFunc("/api/gadgets", GetGadgets).Methods("GET")
@@ -104,6 +93,21 @@ func start(db *bolt.DB, port, root string, iRoot string, lg controllers.Logger) 
 	lg.Printf("listening on %s\n", addr)
 	err := http.ListenAndServe(addr, r)
 	log.Println(err)
+}
+
+//This is the endpoint that the gadgets report to. It is
+//served on a separate port so it doesn't have to be exposed
+//publicly if the main port is exposed.
+func startInternal(iRoot string, lg controllers.Logger) {
+	r := mux.NewRouter()
+	r.HandleFunc("/internal/updates", Relay).Methods("POST")
+	http.Handle(iRoot, r)
+	a := fmt.Sprintf(":%s", os.Getenv("QUIMBY_INTERNAL_PORT"))
+	lg.Printf("listening on %s", a)
+	err := http.ListenAndServe(a, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Ping(w http.ResponseWriter, r *http.Request) {
