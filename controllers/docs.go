9f1c59fac2bc5dd3f5812917f8486863b46385e5
Craig Swank <craigswank@gmail.com>
2015-10-16 13:43:36 +0000

    added edit user and send command

---
+++ controllers/docs.go
@@ -0,0 +1,8 @@
+package controllers
+
+//Options produces documentation for the API
+func Options(args *Args) error {
+	// pth := args.Vars["rest"]
+	// parts := strings.Split(pth, "/")
+	return nil
+}
