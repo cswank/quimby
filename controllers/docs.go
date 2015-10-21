76e45010af78769403409df80c22e38fddbf7465
Craig Swank <craigswank@gmail.com>
2015-10-21 17:50:35 -0600

    Revert "added edit user and send command"
    This reverts commit 9f1c59fac2bc5dd3f5812917f8486863b46385e5.

--- controllers/docs.go
+++
@@ -1,8 +0,0 @@
-package controllers
-
-//Options produces documentation for the API
-func Options(args *Args) error {
-	// pth := args.Vars["rest"]
-	// parts := strings.Split(pth, "/")
-	return nil
-}
