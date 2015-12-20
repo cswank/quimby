e69b929fb8c72ad4bf6db56144114dc282eadb8b
Craig Swank <craigswank@github.com>
2015-12-20 07:53:27 -0700

    hackng build to deal with sucky grunt

--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-cd www
-rm -rf dist
-npm install
-./node_modules/.bin/grunt
-cd ..
-rice embed-go
-godep go install
-rm www-dist.rice-box.go
-cd www
-rm -r dist
-ln -s app dist
-cd ..
+#cd www
+#rm -rf dist
+#npm install
+
+CHART=Chart.js/Chart.js
+CLEANCHART=Chart-js/Chart.js
+sed -i 's|'$CHART'|'$CLEANCHART'|g' app/index.html
+SCATTER=Chart.Scatter.js/Chart.Scatter.js
+CLEANSCATTER=Chart-Scatter-js/Chart.Scatter.js
+
+sed -i 's|'$SCATTER'|'$CLEANSCATTER'|g' app/index.html
+
+mv app/bower_components/Chart.js app/bower_components/Chart-js
+mv app/bower_components/Chart.Scatter.js app/bower_components/Chart-Scatter-js
+
+# ./node_modules/.bin/grunt
+# cd ..
+# rice embed-go
+# godep go build
+# rm www-dist.rice-box.go
+# cd www
+# rm -r dist
+# ln -s app dist
+# cd ..
