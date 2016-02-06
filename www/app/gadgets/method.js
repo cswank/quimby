a17a0b5818b8a2df54a7ada7462181dfd03077f9
Craig Swank <craigswank@gmail.com>
2016-02-06 17:49:13 +0000

    you can now run a method from the browser

--- www/app/gadgets/method.js
+++ www/app/gadgets/method.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('quimby.services')
-    .directive("method", ["$sockets", function($sockets) {
+    .directive("method", ["$sockets", "$methods", "$mdSidenav", function($sockets, $methods, $mdSidenav) {
         return {
             restrict: "E",
             replace: true,
             transclude: true,
             templateUrl: "/gadgets/method.html?t=" + new Date().getTime(),
             scope: {
+                uuid: '=',
                 method: '=',
                 socket: '='
             },
@@ -25,7 +26,31 @@ angular.module('quimby.services')
                     var step = $scope.method.steps[i];
                     return step != undefined && step.indexOf("wait for user") == 0 && i == $scope.method.step;
                 };
+
+                $scope.toggle = function() {
+                    $mdSidenav('new-method').toggle();
+                }
+
+                $scope.close = function () {
+                    $mdSidenav('new-method').close();
+                };
+
+                $scope.run = function () {
+                    $mdSidenav('new-method').close();
+                    $methods.runMethod($scope.uuid, $scope.newMethod);
+                };
             }
+        }
+    }]);
+
+angular.module('quimby.services')
+    .service('$methods', ['$http', function ($http) {
+        this.runMethod = function(id, method) {
+            
+            $http.post(
+                "/api/gadgets/" + id + "/method",
+                {method: method.split("\n")}
+            )
         }
     }]);
 
